64096f22ad7d55995767adc5ac347a89aabb5e3b
your_username <om14789632145@gmail.com>
2020-12-31 14:35:17 +0700

    initial commit

---
+++ api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from .serializers import *
+
+from .stt import sample_recognize
+# Create your views here.
+from django.http import HttpResponse
+
+err_invalid_input = Response(
+    {'message': 'Cannot create user, please recheck input fields'},
+    status=status.HTTP_400_BAD_REQUEST,
+)
+err_no_permission = Response(
+    {'message': 'You do not have permission to perform this action'},
+    status=status.HTTP_403_FORBIDDEN,
+)
+err_not_found = Response(
+    {'message': 'Not found'},
+    status=status.HTTP_404_NOT_FOUND,
+)
+err_not_allowed = Response(
+    {'message': 'Operation Not Allowed'},
+    status=status.HTTP_405_METHOD_NOT_ALLOWED
+)
+
+
+def create_log(user, desc):
+    Log.objects.create(user=user, desc=desc)
