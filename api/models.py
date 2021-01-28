e3c2bf6670a1e688577d441c8cef7065caa8268f
your_username <om14789632145@gmail.com>
2021-01-29 01:52:26 +0700

    change models

--- api/models.py
+++ api/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField,JSONField
 from django.contrib.auth.models import User
+from django.contrib.gis.db.models import PolygonField
 from django.core.validators import RegexValidator, \
     MinValueValidator, MaxValueValidator
 
 # Class project
 class Project(models.Model):
+    TASK_CHOICES = (
+    ('Segmentation', 'Segmentation'),
+    ('Classification', 'Classification'),
+    )
     cover = models.ImageField(upload_to='cover/',null=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    task = models.CharField(max_length=50)
+    # เปลี่ยน task เป็นchoice
+    task = models.CharField(max_length=1,choices=TASK_CHOICES)
+    predclasses = ArrayField(models.CharField(max_length=50))
+    timestamp = models.DateTimeField(auto_now_add=True, )
     users = models.ManyToManyField(
         User,
         related_name='projects',
@@ -19,7 +28,7 @@ class Project(models.Model):
         return self.name
 
     def check_task(self,task):
-        valid_task = ['classification','segmentation']
+        valid_task = ['Classification','Segmentation']
         if task in valid_task:
             return True
         return False
@@ -27,33 +36,41 @@ class Project(models.Model):
     class Meta:
         unique_together = ('name',)
 
-# Class disease
-class Diag(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        unique_together = ('name',)
-    
-
-
 class Pipeline(models.Model):
     name = models.CharField(max_length=50)
     pipeline_id = models.CharField(max_length=40,default="Empty")
     accuracy = models.FloatField(blank=True,default=0.0)
-    project = models.ManyToManyField(
+    description = models.CharField(max_length=500)
+    project = models.ForeignKey(
         Project,
-        related_name='pipelines',
+        related_name='pipeline',
         default=None,
         blank=True,
+        on_delete=models.CASCADE,
     )
     def __str__(self):
         return self.name
     
     class Meta:
         unique_together = ('pipeline_id',)
+
+
+class PredictResult(models.Model):
+    predicted_class = JSONField()
+    predicted_mask = models.BinaryField(blank=True,null=True)
+    predicted_polygon = PolygonField(blank=True,null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, )
+    pipeline = models.ForeignKey(
+        Pipeline,
+        related_name='result',
+        on_delete=models.CASCADE,
+    )
+    image = models.ForeignKey(
+        Pipeline,
+        related_name='result',
+        on_delete=models.CASCADE,
+    )
+    
 
 class Image(models.Model):
     data = models.ImageField(upload_to='image/')
@@ -62,43 +79,19 @@ class Image(models.Model):
     patient_age = models.IntegerField(validators=[MinValueValidator(0), ])
     content_date = models.DateField()
     physician_name = models.CharField(max_length=50)
-    project = models.ManyToManyField(
+    status = models.IntegerField(default=0)
+    actual_class = models.CharField(max_length=50,default='')
+    timestamp = models.DateTimeField(auto_now_add=True, )
+    project = models.ForeignKey(
         Project,
         related_name='images',
         default=None,
         blank=True,
+        on_delete=models.CASCADE,
     )
+    
     def __str__(self):
         return self.data.name
-    
-
-    
-
-class Result(models.Model):
-    project = models.ForeignKey(
-        Project,
-        related_name='result',
-        on_delete=models.CASCADE,
-        default=None
-    )
-    images = models.ForeignKey(
-        Image,
-        related_name='result',
-        on_delete=models.CASCADE,
-        default=None,
-    )
-    diag = models.ForeignKey(
-        Diag,
-        related_name='result',
-        on_delete=models.CASCADE,
-        default=None,
-        null=True,
-        blank=True,
-    )
-    is_verified = models.IntegerField(default=0) # 0:in process , 1: AI-Annotated , 2: Verified
-    note = models.CharField(blank=True,max_length=300)
-    def __str__(self):
-        return f"Project: {self.project.name}, Image: {self.images.data.name} " 
     
 
 class Log(models.Model):
