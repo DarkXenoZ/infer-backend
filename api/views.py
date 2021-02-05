b512303d12469a216846e91ae337920c19e41cc7
your_username <om14789632145@gmail.com>
2021-02-05 17:55:36 +0700

    config infer_image

--- api/views.py
+++ api/views.py
@@ -655,7 +655,8 @@ class ProjectViewSet(viewsets.ModelViewSet):
         file_path = os.path.join("..","media","")
         os.makedirs("tmp", exist_ok=True)
         for img in images:
-            os.symlink(file_path+img, tmp_path+img)
+            img_name = img.split(':')[-1]
+            os.symlink(file_path+img, tmp_path+img_name)
         output1 = subprocess.check_output(
             f"sudo clara create job -n {user.username} {project.name} -p {pipeline.pipeline_id} -f {tmp_path} ", 
             shell=True, 
