fd5a0c83623d04f1d5d5e68c132980e44f2226bd
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2021-02-22 21:11:30 +0700

    allow DB_HOST to be configured from env

--- project/settings.py
+++ project/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-# if os.environ.get('DB_HOST'):
-#     DATABASES['default']['HOST'] = os.environ.get('DB_HOST')
+if os.environ.get('DB_HOST'):
+    DATABASES['default']['HOST'] = os.environ.get('DB_HOST')
 if os.environ.get('POSTGRES_PASSWORD'):
     DATABASES['default']['PASSWORD'] = os.environ.get('POSTGRES_PASSWORD')
 if os.environ.get('POSTGRES_DB'):
