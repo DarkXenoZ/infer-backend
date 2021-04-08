e8b5e5fde6ca4bab0c541ada9145695f789d50a0
your_username <om14789632145@gmail.com>
2021-04-08 21:26:33 +0700

    add pipeline in UserProjectSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -71,9 +71,10 @@ class PipelineSerializer(serializers.ModelSerializer):
 
 class UserProjectSerializer(serializers.ModelSerializer):
     users = OnlyUserSerializer(many=True)
+    pipelines = PipelineSerializer(many=True)
     class Meta:
         model = Project
-        fields = ("id","name","description","users","task","cover","predclasses")
+        fields = ("id","name","description","pipelines","users","task","cover","predclasses")
 
 class UploadImageSerializer(serializers.ModelSerializer):
     class Meta:
