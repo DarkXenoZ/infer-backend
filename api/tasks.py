7e656d9fe6685e0b8adec268e063f0204e7bc148
your_username <om14789632145@gmail.com>
2021-04-23 23:48:47 +0700

    test spec from file locate

--- api/tasks.py
+++ api/tasks.py
@@ -51,9 +51,11 @@ def infer_image(project,pipeline,image,user):
     url = os.getenv('TRTIS_URL')
     tritonClient = grpcclient.InferenceServerClient(url=url)
 
-    preprocess_module_name = "models."+pipeline.model_name + ".preprocess"
-    preprocessModule = importlib.import_module("preprocess",package=preprocess_module_name)
-
+    # preprocess_module_name = "models."+pipeline.model_name + ".preprocess"
+    # preprocessModule = importlib.import_module("preprocess",package=preprocess_module_name)
+    spec = importlib.util.spec_from_file_location("preprocess", "models/"+pipeline.model_name+"/preprocess")
+    preprocessModule = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(preprocessModule)
     preprocessImage = preprocessModule.preprocess(image[0])
     netInput = grpcclient.InferInput(pipeline.netInputname, preprocessImage.shape, "FP32")
     netOutput = grpcclient.InferRequestedOutput(pipeline.netOutputName)
@@ -62,8 +64,11 @@ def infer_image(project,pipeline,image,user):
     Output = Output.as_numpy(pipeline.netOutputName) # output numpy array!
     predResult = PredictResult.objects.get(pipeline=pipeline,image=image)
     
-    postprocess_module_name = "models."+pipeline.model_name + ".postprocess"
-    postprocessModule = importlib.import_module("postprocess",package=postprocess_module_name)
+    # postprocess_module_name = "models."+pipeline.model_name + ".postprocess"
+    # postprocessModule = importlib.import_module("postprocess",package=postprocess_module_name)
+    spec = importlib.util.spec_from_file_location("postprocess", "models/"+pipeline.model_name+"/postprocess")
+    postprocessModule = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(postprocessModule)
     postprocessModule.postprocess(Output,image,predResult)
     q = Queue.objects.get(project=project,pipeline=pipeline,image=image)
     q.delete()
