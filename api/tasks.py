bae3b8d3843e1740c2655c6a7b32f8681751e3e0
your_username <om14789632145@gmail.com>
2021-04-23 23:26:33 +0700

    use django.utils.importlib instead

--- api/tasks.py
+++ api/tasks.py
@@ -18,7 +18,7 @@ import io
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import tritonclient.grpc as grpcclient
 from django.core.files import File
-import importlib
+from django.utils.importlib import import_module
 
 
 def create_log(user, desc):
@@ -52,7 +52,7 @@ def infer_image(project,pipeline,image,user):
     tritonClient = grpcclient.InferenceServerClient(url=url)
 
     preprocess_module_name = "models."+pipeline.model_name + ".preprocess"
-    preprocessModule = importlib.import_module(preprocess_module_name, package=__package__)
+    preprocessModule = import_module(preprocess_module_name)
 
     preprocessImage = preprocessModule.preprocess(image[0])
     netInput = grpcclient.InferInput(pipeline.netInputname, preprocessImage.shape, "FP32")
@@ -63,7 +63,7 @@ def infer_image(project,pipeline,image,user):
     predResult = PredictResult.objects.get(pipeline=pipeline,image=image)
     
     postprocess_module_name = "models."+pipeline.model_name + ".postprocess"
-    postprocessModule = importlib.import_module(postprocess_module_name, package=__package__)
+    postprocessModule = import_module(postprocess_module_name)
     postprocessModule.postprocess(Output,image,predResult)
     q = Queue.objects.get(project=project,pipeline=pipeline,image=image)
     q.delete()
