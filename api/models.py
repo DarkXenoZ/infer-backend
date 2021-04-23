438d258d6f5458a1a67c7665bceb5716000d2293
your_username <om14789632145@gmail.com>
2021-04-23 14:45:38 +0700

    add TRT and Image3D

--- api/models.py
+++ api/models.py
@@ -33,10 +33,18 @@ class Project(models.Model):
         unique_together = ('name',)
 
 class Pipeline(models.Model):
+    MODEL_CHOICES = (
+    ('CLARA', 'CLARA'),
+    ('NON CLARA', 'NON CLARA'),
+    )
+
     name = models.CharField(max_length=50)
-    pipeline_id = models.CharField(max_length=40)
-    operator =  models.CharField(max_length=50)
-    accuracy = models.FloatField(blank=True,default=0.0)
+    pipeline_id = models.CharField(max_length=40,null=True,blank=True)
+    operator =  models.CharField(max_length=50,null=True,blank=True)
+    model_name = models.CharField(max_length=100,null=True,blank=True)
+    model_type = models.CharField(max_length=10,choices=MODEL_CHOICES)
+    netInputname = models.CharField(max_length=50,null=True,blank=True)
+    netOutputname = models.CharField(max_length=50,null=True,blank=True)
     description = models.CharField(max_length=500,default='')
     clara_pipeline_name = models.CharField(max_length=50,null=True,blank=True,default='empty')
     project = models.ForeignKey(
@@ -50,19 +58,12 @@ class Pipeline(models.Model):
         return self.name
     
     class Meta:
-        unique_together = ('pipeline_id',)
+        unique_together = ('pipeline_id','model_name')
 
-class Pair(models.Model):
-    x = models.FloatField()
-    y = models.FloatField()
-
-    def __str__(self):
-        return (self.x,self.y)
     
 class Image(models.Model):
     name = models.CharField(max_length=50)
-    data8 = models.FileField(upload_to='image8/')
-    data16 = models.FileField(upload_to='image16/')
+    data = models.FileField(upload_to='image3D/')
     patient_name = models.CharField(max_length=50)
     patient_id = models.CharField(max_length=12)
     patient_age = models.IntegerField(validators=[MinValueValidator(0), ])
@@ -84,26 +85,56 @@ class Image(models.Model):
     )
     
     def __str__(self):
-        return self.data8.name
+        return self.data.name
+
+class Image3D(models.Model):
+    name = models.CharField(max_length=50)
+    data = ArrayField(models.FileField(upload_to='image8/'))
+    patient_name = models.CharField(max_length=50)
+    patient_id = models.CharField(max_length=12)
+    patient_age = models.IntegerField(validators=[MinValueValidator(0), ])
+    content_date = models.DateField()
+    physician_name = models.CharField(max_length=50)
+    status = models.IntegerField(default=0) # 0:uploaded 1:in process 2:Annotated 3:verified
+    actual_class =ArrayField(models.CharField(max_length=50),blank=True,null=True)
+    predclass = models.CharField(max_length=50,null=True,blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True, )
+    verify_by = models.CharField(max_length=100,blank=True)
+    note = models.CharField(max_length=300,null=True,blank=True,default=" ")
+    project = models.ForeignKey(
+        Project,
+        related_name='images',
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
 
     
 
 class PredictResult(models.Model):
-    gradcam = models.FileField(upload_to='imagegrad/')
+    gradcam = models.FileField(upload_to='imagegrad/',null=True,blank=True)
     predicted_class = models.JSONField(null=True,blank=True)
-    predicted_mask = models.BinaryField(blank=True,null=True)
-    predicted_polygon = ArrayField(ArrayField(models.FloatField(),size=2),null=True)
+    predicted_mask = ArrayField(models.FileField(upload_to='mask/',null=True,blank=True),null=True,blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, )
     pipeline = models.ForeignKey(
         Pipeline,
         related_name='result',
         null=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
     )
     image = models.ForeignKey(
         Image,
         related_name='result',
         null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    image3D = models.ForeignKey(
+        Image3D,
+        related_name='result',
+        null=True,
+        blank=True,
         on_delete=models.SET_NULL,
     )
     class Meta:
@@ -111,7 +142,7 @@ class PredictResult(models.Model):
     
 class Queue(models.Model):
     #JOB_ID Queue
-    job= models.CharField(max_length=50)
+    job= models.CharField(max_length=50,null=True,blank=True)
     project = models.ForeignKey(
         Project,
         related_name='queue',
