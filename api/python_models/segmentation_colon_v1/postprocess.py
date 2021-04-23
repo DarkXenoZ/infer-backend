99d78a444ba1291010c47f40ea53eed84308e853
your_username <om14789632145@gmail.com>
2021-04-24 03:12:47 +0700

    Update postprocess.py

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -3,7 +3,8 @@ from django.core.files import File
 def postprocess(results,image,predResult):
     results = results.squeeze().round()
     results = (1- results)*255
-    cv2.imwrite("/tmp/"+ image,results)
+    x=cv2.imwrite("/tmp/"+ image,results)
+    print(x)
     f= open("/tmp/"+ image,'rb')
     predResult.predicted_mask = File(f)
     predResult.save()
