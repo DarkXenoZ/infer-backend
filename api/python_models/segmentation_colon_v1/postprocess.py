60c3edc8c6ad2215e484ef6837ae77e517dfb62d
your_username <om14789632145@gmail.com>
2021-04-24 03:50:29 +0700

    change to media/mask and append instead of =

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -6,11 +6,12 @@ from django.core.files.storage import FileSystemStorage
 def postprocess(results,image,predResult):
     results = results.squeeze().round()
     results = (1- results)*255
-    os.makedirs("tmp", exist_ok=True)
+    os.makedirs("media/mask", exist_ok=True)
 
     name = image.split('/')[-1]
-    x=imageio.imwrite("tmp/"+ name,results)
-    predResult.predicted_mask = File(open("tmp/"+ name,'rb'))
+    filepath = "media/mask/"+ name
+    imageio.imwrite(filepath,results)
+    predResult.predicted_mask.append(filepath)
     predResult.save()
     image.status = 2
     image.save()
