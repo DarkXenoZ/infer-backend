99cfa4d78d341565d42d45f9b34dadd9cb69c276
your_username <om14789632145@gmail.com>
2021-04-24 03:48:09 +0700

    undo to tmp

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -6,12 +6,11 @@ from django.core.files.storage import FileSystemStorage
 def postprocess(results,image,predResult):
     results = results.squeeze().round()
     results = (1- results)*255
-    os.makedirs("media/mask", exist_ok=True)
+    os.makedirs("tmp", exist_ok=True)
 
     name = image.split('/')[-1]
-    filepath = "media/mask/"+ name
-    imageio.imwrite(filepath,results)
-    predResult.predicted_mask = filepath
+    x=imageio.imwrite("tmp/"+ name,results)
+    predResult.predicted_mask = File(open("tmp/"+ name,'rb'))
     predResult.save()
     image.status = 2
     image.save()
