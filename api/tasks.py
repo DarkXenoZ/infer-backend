0e338d14ab147fc56e83e6bf1a8331ae0f89ebc0
your_username <om14789632145@gmail.com>
2021-04-24 00:56:13 +0700

    print module name

--- api/tasks.py
+++ api/tasks.py
@@ -51,7 +51,8 @@ def infer_image(project,pipeline,image,user):
     url = os.getenv('TRTIS_URL')
     tritonClient = grpcclient.InferenceServerClient(url=url)
 
-    preprocess_module_name = "'python_models."+pipeline.model_name + ".preprocess'"
+    preprocess_module_name = "python_models."+pipeline.model_name + ".preprocess"
+    print(type(preprocess_module_name))
     preprocessModule = importlib.import_module(preprocess_module_name)
     preprocessImage = preprocessModule.preprocess(image[0])
     netInput = grpcclient.InferInput(pipeline.netInputname, preprocessImage.shape, "FP32")
@@ -61,7 +62,7 @@ def infer_image(project,pipeline,image,user):
     Output = Output.as_numpy(pipeline.netOutputName) # output numpy array!
     predResult = PredictResult.objects.get(pipeline=pipeline,image=image)
     
-    postprocess_module_name = "'python_models."+pipeline.model_name + ".postprocess'"
+    postprocess_module_name = "python_models."+pipeline.model_name + ".postprocess"
     postprocessModule = importlib.import_module(postprocess_module_name)
     postprocessModule.postprocess(Output,image,predResult)
     q = Queue.objects.get(project=project,pipeline=pipeline,image=image)
