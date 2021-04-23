202cbfd2a648ebd976388de8d5a7cdb19ad2d217
your_username <om14789632145@gmail.com>
2021-04-24 03:35:51 +0700

    Update postprocess.py

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -1,16 +1,17 @@
 import imageio
 from django.core.files import File
 import os
+from django.core.files.storage import FileSystemStorage
+
 def postprocess(results,image,predResult):
     results = results.squeeze().round()
     results = (1- results)*255
-    print(results)
     os.makedirs("tmp", exist_ok=True)
 
     name = image.split('/')[-1]
     x=imageio.imwrite("tmp/"+ name,results)
-    print(x)
     f= open("tmp/"+ name,'rb')
+    print(f)
     predResult.predicted_mask = File(f)
     predResult.save()
     image.status = 2
