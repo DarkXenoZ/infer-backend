310cafd7c7c999e95a29ec8c31e73b1784b9ccb6
your_username <om14789632145@gmail.com>
2021-04-23 14:45:51 +0700

    add Image 3D

--- api/serializers.py
+++ api/serializers.py
@@ -15,7 +15,18 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = (
-            "id","name","data8","data16",
+            "id","name","data",
+            "patient_name","patient_id",
+            "patient_age","content_date",
+            "physician_name","status",
+            "actual_class","predclass","verify_by","timestamp"
+            )
+
+class Image3DSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image3D
+        fields = (
+            "id","name","data",
             "patient_name","patient_id",
             "patient_age","content_date",
             "physician_name","status",
@@ -54,7 +65,7 @@ class PredictResultSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = PredictResult
-        fields = ("gradcam","pipeline_name","predicted_class","timestamp")
+        fields = ("gradcam","pipeline_name","predicted_class","predicted_mask","timestamp")
 
 class UserSerializer(serializers.ModelSerializer):
     projects = ProjectSerializer(many=True)
@@ -67,7 +78,7 @@ class PipelineSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Pipeline
-        fields = ("id","name","pipeline_id","operator","description","clara_pipeline_name")
+        fields = ("id","name","pipeline_id","operator","description","clara_pipeline_name","model_name","model_type")
 
 class UserProjectSerializer(serializers.ModelSerializer):
     users = OnlyUserSerializer(many=True)
@@ -79,8 +90,11 @@ class UserProjectSerializer(serializers.ModelSerializer):
 class UploadImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ("project","name","data8","data16","patient_name","patient_id","patient_age","content_date","physician_name","status")
-
+        fields = ("project","name","data","patient_name","patient_id","patient_age","content_date","physician_name","status")
+class UploadImage3DSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image3D
+        fields = ("project","name","data","patient_name","patient_id","patient_age","content_date","physician_name","status")
 
 class ImageProjectSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True)
@@ -100,7 +114,26 @@ class ProjectImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = (
-            "id","name","data8","data16",
+            "id","name","data",
+            "patient_name","patient_id",
+            "patient_age","content_date",
+            "physician_name","status","note",
+            "actual_class","verify_by","predclass","timestamp",
+            "project_name","project_task","project_predclasses"
+            )
+
+class ProjectImage3DSerializer(serializers.ModelSerializer):
+    project_name = serializers.CharField(
+        source='project.name'
+    )
+    project_task = serializers.CharField(
+        source='project.task'
+    )
+    project_predclasses = serializers.ListField(child=serializers.CharField(),source='project.predclasses')
+    class Meta:
+        model = Image3D
+        fields = (
+            "id","name","data",
             "patient_name","patient_id",
             "patient_age","content_date",
             "physician_name","status","note",
