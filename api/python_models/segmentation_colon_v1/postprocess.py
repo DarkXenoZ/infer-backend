1a15630e4e4f76e4600a633fbc3fcb53ffd583b3
your_username <om14789632145@gmail.com>
2021-04-27 03:12:43 +0700

    change filepath to filename

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -9,6 +9,7 @@ def postprocess(results,image,predResult):
     results = results.reshape((1,384,288,1))
     results = results.swapaxes(1,2)
     filename = image[0].split('/')[-1]
+    filename = filename.split('.')[0] +".nrrd"
     my_header = {
         'dimension': 4,
         'kinds': ['list', 'domain', 'domain', 'domain'],
@@ -19,8 +20,6 @@ def postprocess(results,image,predResult):
         
     }
     # Write to a NRRD file
-    os.makedirs("tmp", exist_ok=True)
-    filepath = os.path.join("tmp",filename)
-    nrrd.write(filepath, results, my_header)
+    nrrd.write(filename, results, my_header)
     
-    return filepath
+    return filename
