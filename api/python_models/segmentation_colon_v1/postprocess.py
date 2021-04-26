3873052371afd136f7d70dd5bd12db4192cf8fe5
your_username <om14789632145@gmail.com>
2021-04-26 17:03:07 +0700

    write to nrrd instead

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -1,13 +1,26 @@
 import imageio
 from django.core.files import File
 import os
+import nrrd
 
 def postprocess(results,image,predResult):
-    results = results.squeeze().round()
-    results = (1- results)*255
-    os.makedirs("media/mask", exist_ok=True)
-
-    name = image[0].split('/')[-1]
-    filepath = "media/mask/"+ name
-    imageio.imwrite(filepath,results)
+    results = results.round()
+    results = (1- results)
+    results = results.reshape((1,384,288,1))
+    results = results.swapaxes(1,2)
+    filename = image[0].split('/')[-1]
+    my_header = {
+        'dimension': 4,
+        'kinds': ['list', 'domain', 'domain', 'domain'],
+        
+        'Segment0_ID': 'Segment_1',
+        'Segment0_Name': 'Colon',
+        'Segment0_Layer': '0',
+        
+    }
+    # Write to a NRRD file
+    os.makedirs("tmp", exist_ok=True)
+    filepath = os.path.join("tmp",filename)
+    nrrd.write(filepath, results, my_header)
+    
     return filepath
