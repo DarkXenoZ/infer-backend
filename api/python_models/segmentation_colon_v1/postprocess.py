2bbfebd163c6a5f9ac92c65b9bbb70c7ede82033
your_username <om14789632145@gmail.com>
2021-04-27 19:24:11 +0700

    remove swapping

--- api/python_models/segmentation_colon_v1/postprocess.py
+++ api/python_models/segmentation_colon_v1/postprocess.py
@@ -7,9 +7,9 @@ def postprocess(results,image,predResult):
     results = results.round()
     results = (1- results)
     results = results.reshape((1,384,288,1))
-    results = results.swapaxes(1,2)
+    # results = results.swapaxes(1,2)
     filename = image[0].split('/')[-1]
-    filename = filename.split('.')[0] +".nrrd"
+    filename = filename.split('.')[0] +".seg.nrrd"
     my_header = {
         'dimension': 4,
         'kinds': ['list', 'domain', 'domain', 'domain'],
