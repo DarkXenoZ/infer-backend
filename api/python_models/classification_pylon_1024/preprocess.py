2ad41e69146462b0e1b4633ce8232dc2c42945de
DarkXenoZ <phuchit.chadasin@gmail.com>
2021-05-17 20:37:37 +0700

    read image as grayscale for pylon model

--- api/python_models/classification_pylon_1024/preprocess.py
+++ api/python_models/classification_pylon_1024/preprocess.py
@@ -14,6 +14,6 @@ def get_transformed_image(image):
     return transformed_img
 
 def preprocess(Input):
-    img = cv2.imread(os.path.join('/backend/media',Input))
+    img = cv2.imread(os.path.join('/backend/media',Input), cv2.IMREAD_GRAYSCALE)
     transformed_img = get_transformed_image(img)
     return transformed_img
