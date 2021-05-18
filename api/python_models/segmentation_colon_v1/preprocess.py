d97af0cafce5595251ec85e239f799159515916d
your_username <om14789632145@gmail.com>
2021-05-18 11:02:24 +0700

    add comment

--- api/python_models/segmentation_colon_v1/preprocess.py
+++ api/python_models/segmentation_colon_v1/preprocess.py
@@ -1,11 +1,8 @@
 import albumentations as albu
-print("import albu")
+# make sure import cv2 before smp(torchvision)
 import cv2
-print("import cv2")
 import segmentation_models_pytorch as smp
-print("import smp")
 import numpy as np
-print("import np")
 def to_tensor(x, **kwargs):
     return x.transpose(2, 0, 1).astype('float32')
 
@@ -15,14 +12,12 @@ def get_preprocessing_segment_colon(image):
         albu.Lambda(image=smp.encoders.get_preprocessing_fn('resnet34', 'imagenet')),
         albu.Lambda(image=to_tensor, mask=to_tensor),
     ]
-    print("transform")
     transform = albu.Compose(_transform)
     preprocessImage = transform(image=image)["image"]
     preprocessImage = np.expand_dims(preprocessImage,0)
     return preprocessImage
 
 def preprocess(Input):
-    print("preprocess")
     Input = cv2.imread(f"media/{Input}")
     Input = get_preprocessing_segment_colon(Input)
 
